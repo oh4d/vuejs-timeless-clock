6468b55c62345dfaf6bda0588e941940891d3d64
Ohad Goldstein <ohadon3@gmail.com>
2018-12-28 21:51:33 +0200

    add clock functionality, support moment, date object and string

--- TimelessClock.vue
+++ TimelessClock.vue
@@ -2,32 +2,47 @@
     <div class="timeless-clock-container" style="direction: ltr">
         <div class="timeless-clock">
             <span class="timeless-clock-hours">{{ hoursComputed }}</span>
-            <span>:</span>
+            <span :style="{'visibility': (blinked) ? 'hidden' : 'visible'}">:</span>
             <span class="timeless-clock-minutes">{{ minutesComputed }}</span>
-            <span>:</span>
-            <span class="timeless-clock-seconds">{{ secondsComputed }}</span>
+
+            <template v-if="displaySeconds">
+                <span :style="{'visibility': (blinked) ? 'hidden' : 'visible'}">:</span>
+                <span class="timeless-clock-seconds">{{ secondsComputed }}</span>
+            </template>
         </div>
     </div>
 </template>
 
 <script>
+    import moment from 'moment-timezone';
+
+    function padClock(value) {
+        value = Number(value);
+        return (value >= 10) ? value : '0' + value;
+    }
+
     export default {
-        props: {},
+        props: {
+            blink: {default: true},
+            displaySeconds: {default: true},
+            time: {default: null},
+            timezone: {default: null}
+        },
 
         data() {
             return {
                 hours: 0,
                 minutes: 0,
                 seconds: 0,
+                current: null,
                 interval: null,
+                blinked: false
             }
         },
 
         created() {
-            this.start();
+            this.initializeTime();
         },
-
-        mounted() {},
 
         destroyed() {
             this.stop();
@@ -35,26 +50,66 @@
 
         computed: {
             hoursComputed() {
-                return this.clockPart(this.hours);
+                return padClock(this.hours);
             },
 
             minutesComputed() {
-                return this.clockPart(this.minutes);
+                return padClock(this.minutes);
             },
 
             secondsComputed() {
-                return this.clockPart(this.seconds);
+                return padClock(this.seconds);
             }
         },
 
         methods: {
+            initializeTime() {
+                // Default clock time
+                if (!this.time || this.time instanceof Date) {
+                    this.current = (this.time) ? this.time : new Date();
+                    this.defaultUpdate();
+                }
+
+                // moment use
+                else if (this.time === 'moment' || this.time instanceof moment) {
+                    this.initializeMomentTime();
+                }
+
+                // string use
+                else if (typeof this.time === 'string') {
+                    this.current = '';
+                    let splitClockString = this.time.split(':');
+
+                    for (let i = 0; i < 3; i++) {
+                        this.current += (i > 0) ? ':' : '';
+                        this.current += (splitClockString[i]) ? padClock(splitClockString[i]) : '00';
+                    }
+
+                    this.setClockString();
+                }
+
+                if (this.current) {
+                    this.start();
+                }
+            },
+
+            initializeMomentTime() {
+                this.current = (this.time instanceof moment) ? this.time : moment();
+
+                if (this.timezone) {
+                    this.current.tz(this.timezone);
+                }
+
+                this.updateByMoment();
+            },
+
             /**
              * Start the clock
              * Add interval for every second
              */
             start() {
                 this.interval = setInterval(() => {
-                    this.addSecond();
+                    this.intervalClock();
                 }, 1000);
             },
 
@@ -67,6 +122,42 @@
                 this.interval = null;
             },
 
+            /**
+             * Clock Interval Handler
+             */
+            intervalClock() {
+                if (this.blink) {
+                    this.blinked = !this.blinked;
+                }
+
+                if (typeof this.current === 'object') {
+                    this.intervalInstanceClock();
+                    return;
+                }
+
+                // Continue update string clock
+                this.addSecond();
+                this.current = this.hoursComputed + ':' + this.minutesComputed + ':' + this.secondsComputed;
+            },
+
+            intervalInstanceClock() {
+                // Case current time handle with moment object
+                if (this.current instanceof moment) {
+                    this.current.add(1, 'seconds');
+                    this.updateByMoment();
+                    return;
+                }
+
+                // Case current time handle with Date object
+                if (this.current instanceof Date) {
+                    this.current.setSeconds(this.seconds + 1);
+                    this.defaultUpdate();
+                }
+            },
+
+            /**
+             * Manually Add Second
+             */
             addSecond() {
                 if (this.seconds === 59) {
                     this.addMinute();
@@ -77,6 +168,9 @@
                 this.seconds++;
             },
 
+            /**
+             * Manually Add Minute
+             */
             addMinute() {
                 if (this.minutes === 59) {
                     this.addHour();
@@ -87,6 +181,9 @@
                 this.minutes++;
             },
 
+            /**
+             * Manually Add Hour
+             */
             addHour() {
                 if (this.hours === 23) {
                     this.hours = 0;
@@ -96,8 +193,33 @@
                 this.hours++;
             },
 
-            clockPart(value) {
-                return (value >= 10) ? value : '0' + value;
+            /**
+             * Update moment instance clock data
+             */
+            updateByMoment() {
+                this.seconds = this.current.seconds();
+                this.minutes = this.current.minutes();
+                this.hours = this.current.hours();
+            },
+
+            /**
+             * Update default date object
+             */
+            defaultUpdate() {
+                this.seconds = this.current.getSeconds();
+                this.minutes = this.current.getMinutes();
+                this.hours = this.current.getHours();
+            },
+
+            /**
+             *
+             */
+            setClockString() {
+                let splitClockString = this.current.split(':');
+
+                this.hours = Number(splitClockString[0]);
+                this.minutes = Number(splitClockString[1]);
+                this.seconds = Number(splitClockString[2]);
             }
         }
     }
--- example/App.vue
+++ example/App.vue
@@ -1,14 +1,50 @@
 <template>
     <div class="container">
-        <timeless-clock></timeless-clock>
+        <ul>
+            <li>
+                <h2>Local Time<br/><small>default use</small></h2>
+                <timeless-clock :displaySeconds="false"></timeless-clock>
+            </li>
+            <li>
+                <h2>France Timezone<br/><small>moment use</small></h2>
+                <timeless-clock :displaySeconds="false"
+                                timezone="Europe/Paris"
+                                :time="getMomentTime()"></timeless-clock>
+            </li>
+            <li>
+                <h2>America Timezone<br/><small>moment use</small></h2>
+                <timeless-clock :blink="false"
+                                time="moment"
+                                timezone="America/Los_Angeles"></timeless-clock>
+            </li>
+            <li>
+                <h2>Local Time<br/><small>Date object use</small></h2>
+                <timeless-clock></timeless-clock>
+            </li>
+            <li>
+                <h2>Local Time<br/><small>Date object use</small></h2>
+                <timeless-clock :time="new Date()"></timeless-clock>
+            </li>
+            <li>
+                <h2>Custom time as string</h2>
+                <timeless-clock time="23:33:43"></timeless-clock>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
+    import moment from 'moment';
     import TimelessClock from './../TimelessClock.vue';
 
     export default {
-        components: { TimelessClock }
+        components: { TimelessClock },
+
+        methods: {
+            getMomentTime() {
+                return moment();
+            }
+        }
     }
 </script>
 
@@ -29,5 +65,19 @@
     .timeless-clock-container {
         padding: 10px;
         background: #ebebeb;
+        display: inline-block;
+    }
+
+    ul {
+        list-style: none;
+
+        li {
+            width: 300px;
+            padding: 10px;
+            margin: 0 10px;
+            text-align: center;
+            display: inline-block;
+            border:1px solid #ebebeb;
+        }
     }
 </style>
